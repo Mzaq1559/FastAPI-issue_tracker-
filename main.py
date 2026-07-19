0c9308252b2c7857a1bc762ea1b7e7a4601ae096
Mzaq1559 <muhammadzulqarnain1559@gmail.com>
2026-07-19 17:22:33 +0500

    practice

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = [
+    {"id":1, "name": "item one"},
+    {"id":2, "name": "item two"},
+    {"id":3, "name": "item three"},
+    {"id":4, "name": "item four"},
+
+]
 @app.get("/health")
 def health_check():
     return {"status" : "ok"}
+
+@app.get("/items")
+def get_items():
+    return items;
+
+@app.get("/items/{item_id}")
+def get_item_by_id(item_id: int):
+    for item in items:
+        if item["id"] == item_id:
+            return item
+    return {"error": "item not found"}
+
+@app.post("/items")
+def create_items(item: dict):
+    items.append(item)
+    return item
