23acab9f054e0571823829550d4e375362cbdc85
Mzaq1559 <muhammadzulqarnain1559@gmail.com>
2026-08-05 01:19:43 +0500

    added a custom timing middleware

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes.issues import router as issues_router
+from app.middleware.timer import timer_middleware
 
 app = FastAPI()
+app.middleware("http")(timer_middleware)
 app.include_router(issues_router)
