4cffb32b3e190b9d0bf79ecc5572acfe22a8522f
Mzaq1559 <muhammadzulqarnain1559@gmail.com>
2026-08-05 01:24:21 +0500

    added cors to specify methods, domains etc that use API

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes.issues import router as issues_router
 from app.middleware.timer import timer_middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.middleware("http")(timer_middleware)
